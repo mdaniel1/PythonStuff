53fec669a16f147c210dfcf92e6a8d5397050a9b
M. Daniel <46349079+mdaniel1@users.noreply.github.com>
2020-02-18 16:39:04 +0100

    Update FreezeLayer_pre_task_script.py

--- FreezeLayer_pre_task_script.py
+++ FreezeLayer_pre_task_script.py
@@ -1,35 +1,14 @@
+#v1.03, by Daniel Martin
+#in collaboration with Raphael Jouretz
+
 from Deadline.Scripting import *
 import os
-
-def findJob(job, deadlinePlugin):
-    currentJob = None
-    
-    deadlinePlugin.LogInfo("Looking for current job...")
-    AllJobs = RepositoryUtils.GetJobs(True)
-    for j in AllJobs:
-        if str(job) in j.JobName:
-            deadlinePlugin.LogInfo("Job found, name is " + str(job))
-            currentJob = RepositoryUtils.GetJob(j.JobId, True)
-    return currentJob
-
-def getFramesForJob(job):
-    frames = job.JobFrames.split("-")
-    framesNum = (int(frames[1])-int(frames[0]))+1 #+1 because the frame 100 is included
-    return framesNum
 
 def __main__(*args):
     deadlinePlugin = args[0]
     deadlinePlugin.LogInfo("---[STARTING PRE-TASK SCRIPT]---")
     deadlinePlugin.LogStdout("OS is " + os.name)
     deadlinePlugin.LogStdout("DEBUG : Current task ID is {}".format(deadlinePlugin.GetCurrentTaskId()))
-    
-    #These are used for debugging
-    #job = deadlinePlugin.GetJob()
-    #currentJob = findJob(job, deadlinePlugin)
-    #outputDirectory = currentJob.JobOutputDirectories[0]
-    #outputName = currentJob.JobOutputFileNames[0]
-    #for number in range(100, getFramesForJob(currentJob)+101):
-    #        deadlinePlugin.LogStdout(str(number))
     
     if deadlinePlugin.GetCurrentTaskId() != "0":
         deadlinePlugin.LogStdout("This task isn't the first frame, setting to complete")
